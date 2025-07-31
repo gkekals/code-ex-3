838c8d3a9f3fc20b1ab694f1a4a190cf1bb68afc
gkekals <gkekals12@kbu.ac.kr>
2025-07-31 14:45:20 +0900

    1

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector('#container')
+const title = document.querySelector('.title')
+
+const playBtn = document.querySelector('#play')
+const stopBtn = document.querySelector('#stop')
+
+let play = setInterval(bgChange, 1000)
+
+
+function getRandomColor() {
+    let hex = "#" + Math.random().toString(16).slice(2, 8)
+
+
+    return hex
+}
+function bgChange() {
+    let randomColor = getRandomColor()
+    container.style.backgroundColor = randomColor
+    title.textContent = randomColor
+}
+
+
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(play)
+    play = null
+
+})
+playBtn.addEventListener('click', () => {
+    if (!play) {
+        play = setInterval(bgChange, 1000)
+    }
+})
